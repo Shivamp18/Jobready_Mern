a51bb2044ee21d53b53874faa6d7297bb69e2911
Shivamp18 <148711217+Shivamp18@users.noreply.github.com>
2025-09-07 18:05:01 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -41,6 +41,10 @@ app.use(cors({
 }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use((req, res, next) => {
+  console.log(`Incoming Request: ${req.method} ${req.url}`);
+  next();
+});
 
 // Database connection
 mongoose.connect(process.env.MONGO_URI)
