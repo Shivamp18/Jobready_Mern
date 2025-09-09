aca73411e4bf7ef3e8cd19e357f1d9e1ddd345c8
Shivamp18 <148711217+Shivamp18@users.noreply.github.com>
2025-09-10 00:11:49 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -71,7 +71,7 @@ app.use('/api/feedback', feedbackRoutes);
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
   
-  app.get('*', (req, res) => {
+  app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
   });
 }
