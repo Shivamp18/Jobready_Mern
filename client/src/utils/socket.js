7517dcd2258b608f601e23147fab928b7b706c9e
Shivamp18 <148711217+Shivamp18@users.noreply.github.com>
2025-09-09 22:53:54 +0530

    Update socket.js

--- client/src/utils/socket.js
+++ client/src/utils/socket.js
@@ -1,7 +1,10 @@
 
 import { io } from "socket.io-client";
 
+console.log('Type of io:', typeof io);
 const socketUrl = process.env.REACT_APP_SOCKET_URL || "http://localhost:5000";
+console.log('Socket URL:', socketUrl);
 const socket = io(socketUrl);
+console.log('Socket object after initialization:', socket);
 
 export default socket;
